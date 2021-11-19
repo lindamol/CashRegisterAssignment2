5c678582fe1718d147d12b90e37e95e173384ca3
lindamol <lindamoljosem@gmail.com>
2021-11-18 20:35:07 -0500

    version2

--- app/src/main/java/com/example/cashregisterassignment2/HistoryActivity.java
+++ app/src/main/java/com/example/cashregisterassignment2/HistoryActivity.java
@@ -6,27 +6,36 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
  RecyclerView historyRecyclerview;
+ TextView historytextview;
  ArrayList<Historylist> myhistorylist = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
         Intent frommangerintent = getIntent();
+        historytextview = findViewById(R.id.historytextView);
+        historytextview.setVisibility(View.INVISIBLE);
         historyRecyclerview = findViewById(R.id.historyRecyclerview);
         historyRecyclerview.setLayoutManager(new LinearLayoutManager(this));
-        if(!(this.getIntent().getExtras().getParcelableArrayList("forhistroryActivity") == null))
+        if(!(this.getIntent().getExtras().getParcelableArrayList("forhistroryActivity").isEmpty()))
         {
             myhistorylist = this.getIntent().getExtras().getParcelableArrayList("forhistroryActivity");
-            System.out.println(myhistorylist);
+            //System.out.println(myhistorylist);
             HistoryAdapter adapter = new HistoryAdapter(this, myhistorylist);
             historyRecyclerview.setAdapter(adapter);
         }
-        else {System.out.println("Empty History");}
+        else {System.out.println("Empty History");
+           historytextview.setText("NO PURCHASE HISTORY");
+           historytextview.setVisibility(View.VISIBLE);
+           historyRecyclerview.setVisibility(View.INVISIBLE);
+        }
 //        System.out.println("Printing History in HistoryActivity:");
 //
     }
--- app/src/main/java/com/example/cashregisterassignment2/ManagerActivity.java
+++ app/src/main/java/com/example/cashregisterassignment2/ManagerActivity.java
@@ -12,27 +12,28 @@ import java.util.ArrayList;
 public class ManagerActivity extends AppCompatActivity {
     Button buttonHistory;
     ArrayList<Historylist> myhistoryarray = new ArrayList<>();
+
         @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_manager);
         Intent frommainintent = getIntent();
-       if(!(this.getIntent().getExtras().getParcelableArrayList("history") == null))
-       {
-           myhistoryarray = this.getIntent().getExtras().getParcelableArrayList("history");
-       }
-       else {System.out.println("Empty History");}
-        System.out.println("Printing History in ManagerPanel:");
-        System.out.println(myhistoryarray);
         buttonHistory = findViewById(R.id.history);
         buttonHistory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(!(frommainintent.getExtras().getParcelableArrayList("history")).isEmpty())
+                {    myhistoryarray = frommainintent.getExtras().getParcelableArrayList("history");
+                   // myhistoryarray = this.getIntent().getExtras().getParcelableArrayList("history");
+                }
+                else {System.out.println("Empty History");
+                                    }
+//                System.out.println("Printing History in ManagerPanel:");
+//                System.out.println(myhistoryarray);
                 openHistoryActivity();
             }
         });
             }
-
     private void openHistoryActivity() {
         Intent historyintent = new Intent(this,HistoryActivity.class);
         Bundle bundlehistory = new Bundle();
